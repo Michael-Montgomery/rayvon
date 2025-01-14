ad88fa2742f1dae2d0df2d79e2de195ca586baaa
Michael Montgomery <michaelmontgomery@Michaels-MacBook-Pro.local>
2025-01-14 12:16:28 -0500

    things and stuff.

---
+++ data/episodes.js
@@ -0,0 +1,34 @@
+let episodes = [
+    {
+        title: 'title of this episode',
+        runtimeMinutes: 36,
+        runtimeSeconds: 14
+    },
+    {
+        title: 'title of this episode',
+        runtimeMinutes: 36,
+        runtimeSeconds: 14
+    },
+    {
+        title: 'title of this episode',
+        runtimeMinutes: 36,
+        runtimeSeconds: 14
+    },
+    {
+        title: 'title of this episode',
+        runtimeMinutes: 36,
+        runtimeSeconds: 14
+    },
+    {
+        title: 'title of this episode',
+        runtimeMinutes: 36,
+        runtimeSeconds: 14
+    },
+    {
+        title: 'title of this episode',
+        runtimeMinutes: 36,
+        runtimeSeconds: 14
+    }
+]
+
+export default episodes;
